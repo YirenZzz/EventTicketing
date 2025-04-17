7124d1109c4643e5f4c1cb1f7e987c09aff6f3a0
YirenZzz <zhaoyirendaisy@icloud.com>
2025-04-16 23:40:14 -0400

    Added Websocket for real-time update ticket purchase information for both organizer and attendee.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,41 +1,14 @@
-//已改
+// app/layout.tsx
+import './globals.css';
+import { Inter } from 'next/font/google';
+import Providers from '../components/Providers';
+import SocketInit from '@/components/SocketInit'; 
 
-// // app/layout.tsx
-// import './globals.css'; // 你可以放置Tailwind或其他css
-// import { Inter } from 'next/font/google';
-
-// const inter = Inter({ subsets: ['latin'] });
-
-// export const metadata = {
-//   title: 'My Event Platform',
-//   description: 'Minimal Next.js + Prisma Setup',
-// };
-
-// export default function RootLayout({ children }: { children: React.ReactNode }) {
-//   return (
-//     <html lang="en" className={inter.className}>
-//       <body>{children}</body>
-//     </html>
-//   );
-// }
-
-
-
-
-
-
-
-
-
-import "./globals.css";
-import { Inter } from "next/font/google";
-import Providers from "../components/Providers";  // 注意路径根据你的项目结构而定
-
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "Ticket.Events",
-  description: "Event Ticketing and Check-in System",
+  title: 'Ticket.Events',
+  description: 'Event Ticketing and Check-in System',
 };
 
 export default function RootLayout({
@@ -46,11 +19,11 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.className}>
       <body>
-        {/* 包裹 SessionProvider 供全局使用 */}
-        <Providers>{children}</Providers>
+        <Providers>
+          <SocketInit />
+          {children}
+        </Providers>
       </body>
     </html>
   );
 }
-
-
