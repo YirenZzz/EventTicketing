3c4527ecbebbfcc6b84d5dfc46c662e772d28ebd
YUTING ZHANG <ytlluvia.zhang@mail.utoronto.ca>
2025-04-17 21:13:41 -0400

    Added prisma.ts for cloud storage

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,19 +1,12 @@
-//已改
+import { PrismaClient } from '@prisma/client';
 
-// lib/prisma.ts
-import { PrismaClient } from "@prisma/client";
-
+// Prevent multiple instances of Prisma Client in development
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-let prisma: PrismaClient;
+export const prisma = globalForPrisma.prisma || new PrismaClient({
+  log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+});
 
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!globalForPrisma.prisma) {
-    globalForPrisma.prisma = new PrismaClient();
-  }
-  prisma = globalForPrisma.prisma;
-}
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
 export default prisma;
