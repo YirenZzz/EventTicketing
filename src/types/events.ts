00a5b0c65abe61f98d5741fd05ae96aaa13fa808
Yiningww <wangyining0609@gmail.com>
2025-04-20 14:04:45 -0400

    Update events.ts

--- src/types/events.ts
+++ src/types/events.ts
@@ -1,10 +1,10 @@
 export interface EventWithDetails extends Event {
-    organizer?: UserProfile;
-    ticketTypes?: Array<any>;
-    customFields?: Array<CustomField>;
-    virtualEventUrl?: string; // For online events (Zoom, Teams, etc.)
-    _count?: {
-      registrations: number;
-    };
-    dynamicStatus?: EventDynamicStatus;
-  }
+  organizer?: UserProfile;
+  ticketTypes?: Array<any>;
+  customFields?: Array<CustomField>;
+  virtualEventUrl?: string; // For online events (Zoom, Teams, etc.)
+  _count?: {
+    registrations: number;
+  };
+  dynamicStatus?: EventDynamicStatus;
+}
