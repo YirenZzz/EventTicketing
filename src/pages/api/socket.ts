f74f5133d715400a1f0cd886fb7b6ac774f0eb45
Yiningww <wangyining0609@gmail.com>
2025-04-20 13:58:59 -0400

    Update socket.ts

--- src/pages/api/socket.ts
+++ src/pages/api/socket.ts
@@ -1,18 +1,18 @@
 // pages/api/socket.ts
-import { Server as IOServer } from 'socket.io';
+import { Server as IOServer } from "socket.io";
 
 let io: IOServer | null = null;
 
 export default function handler(req: any, res: any) {
   if (!res.socket.server.io) {
     const ioServer = new IOServer(res.socket.server, {
-      path: '/api/socket_io', // 必须和客户端 path 一致
+      path: "/api/socket_io", // consistent w/ client side
       addTrailingSlash: false,
     });
     res.socket.server.io = ioServer;
 
-    ioServer.on('connection', socket => {
-      console.log('Socket connected:', socket.id);
+    ioServer.on("connection", (socket) => {
+      console.log("Socket connected:", socket.id);
     });
   }
 
